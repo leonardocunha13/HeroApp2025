338bd6b6ac098173c7b41480d94c7df8e40f743e
HeroEngAu <mlevkovsky@heroengineering.com.au>
2025-03-28 17:37:16 +0800

    using tabs instead of sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
 import { useAuthenticator } from '@aws-amplify/ui-react'
-import { Sidebar, Menu, MenuItem, SubMenu} from 'react-pro-sidebar';
-import { Link } from 'react-router-dom';
+//import { Sidebar, Menu, MenuItem, SubMenu} from 'react-pro-sidebar';
+//import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Tabs, Button } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 
 function App() {
   const { signOut } = useAuthenticator();
+  const [tab, setTab] = useState('1');
   return (
+    <>
+    <Tabs
+      value={tab}
+      onValueChange={(tab) => setTab(tab)}
+      items={[
+        {
+          label: 'First',
+          value: '1',
+          content: 'Content of the first tab',
+        },
+        {
+          label: 'Second',
+          value: '2',
+          content: (
+            <>
+              <p>Content of the second tab.</p>
+              <Button isFullWidth onClick={() => setTab('1')}>
+                Go to first tab
+              </Button>
+            </>
+          ),
+        },
+      ]}
+    />
+    <button onClick={signOut}>Sign out</button>
+    
+    </>
+    
+    
+    
+  );
+
+ /* return (
     <>
       <div>
         <Sidebar>
@@ -24,6 +60,6 @@ function App() {
       </div>
       <button onClick={signOut}>Sign out</button>
     </>
-  );
+  );*/
 }
 export default App;
