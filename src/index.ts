b4e57beaeb20e2f41349ae43e28e6bae9a6a7ead
HeroEngAu <mlevkovsky@heroengineering.com.au>
2025-03-28 17:03:14 +0800

    removed all Sidebar folders

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './components/Sidebar/Sidebar';
-export * from './components/Sidebar/Menu';
-export * from './components/Sidebar/SubMenu';
-export * from './components/Sidebar/MenuItem';
-export * from './components/Sidebar/ProSidebarProvider';
-export * from './hooks/useProSidebar';
-export * from './components/utils/utilityClasses';
-export type { CSSObject } from '@emotion/styled';
